9b8acecd6e8d2425c5a8023620dc1bc19acc5fe9
xxnislamnxx <darian9888@mail.ru>
2023-11-18 22:33:48 +0700

    Update AppRouter.js

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -19,25 +19,3 @@ const AppRouter = () => {
 };
 export default AppRouter;
 
-
-/*
-import React from "react";
-import { Switch, Route } from 'react-router-dom';
-import { authRoutes, publicRoutes } from "../routes"; // исправлена опечатка
-
-const AppRouter = () => {
-    const isAuth = false;
-    return (
-        <Switch>
-            {isAuth && authRoutes.map(({ path, Component }) => 
-                <Route key={path} path={path} component={Component} exact/> // исправлено на "component"
-            )}
-            {publicRoutes.map(({ path, Component }) => // исправлена опечатка
-                <Route key={path} path={path} component={Component} exact/> // исправлено на "component"
-            )}
-        </Switch>
-    );
-};
-
-export default AppRouter;
-*/
