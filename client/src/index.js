65aebe4c66a0b2d713817c0b4c102a82c68314eb
xxnislamnxx <darian9888@mail.ru>
2023-11-20 22:09:25 +0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React,{createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index';
 import App from './App.js';
+import UserStore from './store/UserStore.js';
 
 
 
@@ -21,11 +22,15 @@ root.render(
       user: new UserStore()
   }}>
     <App />
-  </Context.Provider>,
-{/*<React.StrictMode>
-    <App />
-</React.StrictMode>*/}
+  </Context.Provider>
 );
+/* рабочий вариант
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(  
+<React.StrictMode>
+    <App />
+</React.StrictMode>
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
