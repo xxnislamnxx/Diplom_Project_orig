879c13976383e81fdfea3dc4c93c56f3e66fb0fc
xxnislamnxx <darian9888@mail.ru>
2023-11-27 02:38:06 +0700

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from "./components/AppRouter"
 import NavBar from './components/NavBar.js';
+import Statistics from './components/Statistics.js';
 const App = () => {
   return (
       <BrowserRouter>
         <NavBar />
+        <Statistics />
         <AppRouter />
       </BrowserRouter>
       
