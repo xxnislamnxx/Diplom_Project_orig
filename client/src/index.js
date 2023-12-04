0784801932f0657122b4c918bd6f6fa13d03149e
xxnislamnxx <darian9888@mail.ru>
2023-12-04 23:42:15 +0300

    upd

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import OtdelStore from './store/OtdelStore.js';
 
 export const Context = createContext(null)
 //import reportWebVitals from './reportWebVitals';
-console.log(process.env.REACT_APP_API_URL)
+console.log("Какая то ссылка", process.env.REACT_APP)
 /*
 ReactDOM.render(
   <App />,
