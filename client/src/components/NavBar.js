0ef2f4abd6c3820cddd19cf5793c19cce6657527
xxnislamnxx <darian9888@mail.ru>
2023-12-16 07:55:09 +0300

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,26 +8,35 @@ import { ADMIN_ROUTE, LOGIN_ROUTE, PROJECT_ROUTE, USERLIST_ROUTE } from '../util
 import {Button} from 'react-bootstrap'
 import {observer} from 'mobx-react-lite'
 import {useHistory} from "react-router-dom/cjs/react-router-dom";
-
+import {jwtDecode} from "jwt-decode";
 
 
  const NavBar = observer(()  => {
     const {user} = useContext(Context)
+    const {otdel} = useContext(Context)
     const history = useHistory()
-
+    const token = jwtDecode(localStorage.getItem('token')).Name
+    {/*const otdels = async () => {
+      otdel.setSelectedOtdel
+    }*/}
     const logOut =() => {
     user.setUser({})
     user.setIsAuth(false)
-    
     }
-
-
     return (
+      
       <Navbar bg="dark" data-bs-theme="dark">
         <Container >
           <NavLink style={{color: 'white'}} to={USERLIST_ROUTE}>My Project</NavLink> 
           {user.isAuth ?
             <Nav className="mr-2" style={{color:'white'}}>
+
+              <label 
+                className="d-flex align-items-center"
+                style={{marginRight: "20px"}}
+                >
+                  {token}</label>
+
               <Button 
                 variant={"outline-light"} 
                 onClick={() => history.push(ADMIN_ROUTE)}>Админ панель</Button>
@@ -40,6 +49,7 @@ import {useHistory} from "react-router-dom/cjs/react-router-dom";
                 variant={"outline-light"} 
                 style={{marginLeft: "20px"}}
                 onClick={() => logOut()}>Выйти</Button>
+                
             </Nav>
             :
             <Nav className="ml-auto" style={{color:'white'}}>
